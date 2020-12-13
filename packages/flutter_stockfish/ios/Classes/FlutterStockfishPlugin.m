3256da4c6ef8e1735e876787836968c7470ca6a1
Dao Hoang Son <daohoangson@gmail.com>
2020-12-13 21:49:20 +0700

    flutter create --template=plugin --org=com.daohoangson --project-name=flutter_stockfish --platforms=android,ios --android-language java --ios-language objc .

---
+++ packages/flutter_stockfish/ios/Classes/FlutterStockfishPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterStockfishPlugin.h"
+
+@implementation FlutterStockfishPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_stockfish"
+            binaryMessenger:[registrar messenger]];
+  FlutterStockfishPlugin* instance = [[FlutterStockfishPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
