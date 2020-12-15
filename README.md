54fcd1dbe0f60c68706a4aae40e58866f31b1060
Dao Hoang Son <daohoangson@gmail.com>
2020-12-15 13:49:46 +0700

    Verify Stockfish cpp files can be built

---
+++ README.md
@@ -0,0 +1,7 @@
+# Flutter Stockfish
+
+## Requirements
+
+### iOS
+
+- IPHONEOS_DEPLOYMENT_TARGET = 11.0;
---
+++ scripts/download-nnue.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -eou pipefail
+
+cd packages/flutter_stockfish/ios/Stockfish/src
+make net
